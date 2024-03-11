e610bcbb477019b0389ba90b7ed920c478cf39c8
Ngoc Anh <nguyenthingocanh2307204@gmail.com>
2024-03-11 20:21:26 +0700

    sua bt thuc hanh

--- Lab2/src/App.java
+++ Lab2/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Khong phai phuong trinh bac nhat");
+        System.out.println("Hello, World!");
     }
 }
--- Lab2/src/Bai2.java
+++ Lab2/src/Bai2.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class Bai2 {
     public static void main(String[] args) {
         int x;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Nhap x = ");
-        x=sc.nextInt();
-        if (x%2 == 0) {
+        x = sc.nextInt();
+        if (x % 2 == 0) {
             System.out.print("x la so chan ");
         } else {
             System.out.print("x la so le ");
